dff3b09f0cf7c60a40dd5439fe4641f456eb33ed
Little Seahorse <vytas@littleseahorsemedia.co.uk>
2020-09-15 14:11:28 +0100

    Some small changes.

--- app/components/App.js
+++ app/components/App.js
@@ -12,7 +12,7 @@ var App = React.createClass({
     return (
       <div className="main-app">
         <header className="main-header">
-          <h1>Public Repositories!</h1>
+          <h1>Public Repositories</h1>
         </header>
         <main className="main-content">{this.props.children}</main>
       </div>
